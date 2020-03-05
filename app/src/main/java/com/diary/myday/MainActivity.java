ac1df23169ef17acfb4ee71fd811a1b0d43261f8
Salvist <45643277+Salvist@users.noreply.github.com>
2020-03-04 22:59:41 -0500

    Added Day XX format. It takes current date of the phone and convert to Day XX.
    Important to know that this will not show in Android Studio Editor, so to check you have to run the application in your AVD

--- app/src/main/java/com/diary/myday/MainActivity.java
+++ app/src/main/java/com/diary/myday/MainActivity.java
@@ -1,8 +1,13 @@
 package com.diary.myday;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        TextView dateTimeDisplay;
+        Calendar calendar;
+        SimpleDateFormat dateFormat;
+        String date;
+
+        dateTimeDisplay = (TextView) findViewById(R.id.dayIndicator);
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("dd");
+        date = dateFormat.format(calendar.getTime());
+        if(date.charAt(0) == '0'){
+            date = date.substring(1);
+        }
+        dateTimeDisplay.setText("Day " + date);
+    }
 }
