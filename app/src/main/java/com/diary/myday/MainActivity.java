7fb9fd4c7756d32e0c42ace1eeaf62187b765ac2
Salvist <45643277+Salvist@users.noreply.github.com>
2020-04-06 22:52:18 -0400

    Added open_diary.xml. STILL IN PROGRESS TO SAVE AND LOAD DIARIES

--- app/src/main/java/com/diary/myday/MainActivity.java
+++ app/src/main/java/com/diary/myday/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -20,9 +22,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String FILE_NAME = "text.txt";
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -34,6 +37,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void menu(View v) {
+        setContentView(R.layout.activity_main);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @SuppressLint("SimpleDateFormat")
+    public void create_diary(View v){
+        setContentView(R.layout.create_diary);
+/*
+        TextView dateTimeDisplay;
+        Calendar calendar;
+        SimpleDateFormat dateFormat;
+        String date;
+        dateTimeDisplay = (TextView) findViewById(R.id.dayIndicator);
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("dd");
+        date = dateFormat.format(calendar.getTime());
+        if(date.charAt(0) == '0'){
+            date = date.substring(1);
+        }
+        date = "Day " + date;
+        dateTimeDisplay.setText(date);
+        //save edit text
+
+        */
+
+        TextView dateTimeDisplay;
+        Calendar calendar;
+        SimpleDateFormat dateFormat;
+        String date;
+        int day;
+        String dayCount;
+        dateTimeDisplay = (TextView) findViewById(R.id.dayIndicator);
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("MMM dd yyyy");
+        date = dateFormat.format(calendar.getTime());
+        day = calendar.get(Calendar.DAY_OF_YEAR);
+        dayCount = "Day "+ day + "\t" + date;
+        dateTimeDisplay.setText(dayCount);
+
+    }
+
+    private static final String FILE_NAME = "text.txt";
     public void save(View v) throws IOException {
         EditText dEditText = findViewById(R.id.diary);
         String text = dEditText.getText().toString();
@@ -89,49 +135,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void menu(View v) {
-        setContentView(R.layout.activity_main);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    @SuppressLint("SimpleDateFormat")
-    public void create_diary(View v){
-        setContentView(R.layout.create_diary);
-/*
-        TextView dateTimeDisplay;
-        Calendar calendar;
-        SimpleDateFormat dateFormat;
-        String date;
-        dateTimeDisplay = (TextView) findViewById(R.id.dayIndicator);
-        calendar = Calendar.getInstance();
-        dateFormat = new SimpleDateFormat("dd");
-        date = dateFormat.format(calendar.getTime());
-        if(date.charAt(0) == '0'){
-            date = date.substring(1);
-        }
-        date = "Day " + date;
-        dateTimeDisplay.setText(date);
-        //save edit text
-
-        */
-
-        TextView dateTimeDisplay;
-        Calendar calendar;
-        SimpleDateFormat dateFormat;
-        String date;
-        int day;
-        String dayCount;
-        dateTimeDisplay = (TextView) findViewById(R.id.dayIndicator);
-        calendar = Calendar.getInstance();
-        dateFormat = new SimpleDateFormat("MMM dd yyyy");
-        date = dateFormat.format(calendar.getTime());
-        day = calendar.get(Calendar.DAY_OF_YEAR);
-        dayCount = "Day "+ day + "\t" + date;
-        dateTimeDisplay.setText(dayCount);
-
-    }
-
-
     public void mood(View v){
         setContentView(R.layout.mood_layout);
 
@@ -148,15 +151,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-        public void okay(View view){
-          //  Button okayBtn = (Button) findViewById(R.id.okayBtn);
-            setContentView(R.layout.activity_main);
-            //go into the diary box page
-        }
+    public void okay(View view){
+        //  Button okayBtn = (Button) findViewById(R.id.okayBtn);
+        setContentView(R.layout.activity_main);
+        //go into the diary box page
+    }
 
-        public void sad(View view){
+    public void sad(View view){
         Button sadBtn = (Button) findViewById(R.id.sadBtn);
         //go into minigame
-        }
+    }
 
+    OpenDiaryAdapter adapter;
+    public void open_diary(View view){
+        setContentView(R.layout.open_diary);
+
+        ArrayList<String> diaries = new ArrayList<>();
+        diaries.add("diary 1");
+        diaries.add("diary 2");
+
+        RecyclerView rvPages = (RecyclerView) findViewById(R.id.diary_list);
+        rvPages.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new OpenDiaryAdapter(this, diaries);
+        //adapter.setClickListener( this);
+        rvPages.setAdapter(adapter);
+    }
+
+    public void onitemClick(View view, int position){
+        Toast.makeText(this, "You clicked " + adapter. getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
 }
---
+++ app/src/main/java/com/diary/myday/OpenDiaryAdapter.java
@@ -0,0 +1,66 @@
+package com.diary.myday;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class OpenDiaryAdapter extends RecyclerView.Adapter<OpenDiaryAdapter.ViewHolder>{
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    OpenDiaryAdapter(Context context, List<String> data){
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.diary_pages, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position){
+        String page = mData.get(position);
+        holder.textView.setText(page);
+    }
+
+    @Override
+    public int getItemCount(){
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView textView;
+        ViewHolder(View itemView){
+            super(itemView);
+            textView = itemView.findViewById(R.id.page_date);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view){
+            if(mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    String getItem(int id){
+        return mData.get(id);
+    }
+
+    void setClickListener(ItemClickListener itemClickListener){
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
